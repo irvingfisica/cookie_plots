09f01a0abea7c66d4fb2acdf604f9e7ae53c2492
Irving Morales <irvingfisica@gmail.com>
2024-02-23 15:09:36 -0600

    prueba datos
    prueba para ver si lee la ruta de los datos

--- {{cookiecutter.nombre}}/js/script.js
+++ {{cookiecutter.nombre}}/js/script.js
@@ -1,4 +1,13 @@
 const div = document.querySelector("#graf");
 
-const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
+//const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
+
+const data = await d3.csv("{{cookicutter.data}}");
+
+const plot = Plot.plot({
+    marks: [
+        Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random}))
+    ]
+});
+
 div.append(plot);
