90c3ed4e77594fc8fbe99709204ea7b100831e9e
Irving Morales <irvingfisica@gmail.com>
2024-02-23 15:14:29 -0600

    prueba 2
    prueba 2

--- {{cookiecutter.nombre}}/js/script.js
+++ {{cookiecutter.nombre}}/js/script.js
@@ -2,7 +2,7 @@ const div = document.querySelector("#graf");
 
 //const plot = Plot.rectY({length: 10000}, Plot.binX({y: "count"}, {x: Math.random})).plot();
 
-const data = await d3.csv("{{cookicutter.data}}");
+const data = await d3.csv("");
 
 const plot = Plot.plot({
     marks: [
